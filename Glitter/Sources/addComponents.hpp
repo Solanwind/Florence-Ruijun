397ee8e7a3ca01a1469cd013260215b69d4fcc16
Solanwind <951717201@qq.com>
2018-12-13 17:02:39 +0100

    Update addComponents.hpp

--- Glitter/Sources/addComponents.hpp
+++ Glitter/Sources/addComponents.hpp
@@ -8,9 +8,11 @@
 class AddComponents {
 public:
 	//btRigidBody* addSphere(float rad, float x, float y, float z, float mass);
-	btRigidBody* addSphere(float rad, Model model, btMatrix3x3 rotation, btVector3 translation, float mass);
+	btRigidBody* AddComponents::addSphere(float rad, float x, float y, float z, float mass);
+	glm::mat4 renderSphere(btRigidBody* sphere, glm::vec3 mScale);
 	void addGround();
 	void init();
+	void simulation();
 	
 };
 
